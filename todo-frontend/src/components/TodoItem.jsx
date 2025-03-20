737e03476a3198de81a765c9f64ad37c63e09a75
Steve-Anson <steveanson12@gmail.com>
2025-03-20 21:55:21 +0530

    Connect React App with Get All Todos REST API

--- todo-frontend/src/components/TodoItem.jsx
+++ todo-frontend/src/components/TodoItem.jsx
@@ -1,17 +1,13 @@
 const TodoItem = ({ todo }) => {
   return (
-    <div className="flex border-4 mb-2 rounded-md font-semibold">
-      <h2 className="border-2 py-4 px-4 w-1/4 bg-amber-200 text-lg">
-        {todo.title}
-      </h2>
-      <p className="border-2 py-4 px-4  w-1/2 bg-amber-200 text-lg">
+    <div className="flex border-4 mb-2 rounded-md font-semibold text-md">
+      <h2 className="border-2 py-4 px-4 w-1/4 bg-amber-200 ">{todo.title}</h2>
+      <p className="border-2 py-4 px-4  w-1/2 bg-amber-200">
         {todo.description}
       </p>
-      <p className="border-2 py-4 px-4 w-1/6 bg-amber-200 text-lg">
-        {todo.dueDate}
-      </p>
+      <p className="border-2 py-4 px-4 w-1/6 bg-amber-200">{todo.dueDate}</p>
       <p
-        className={`border-2 text-center py-4 px-4 w-1/10 text-lg ${
+        className={`border-2 text-center py-4 px-4 w-1/10 ${
           todo.completed ? "bg-green-500" : "bg-red-500"
         }`}
       >
--- todo-frontend/src/components/TodoList.jsx
+++ todo-frontend/src/components/TodoList.jsx
@@ -1,43 +1,19 @@
+import { useState, useEffect } from "react";
+import { getAllTodos } from "../services/TodoService";
 import TodoItem from "./TodoItem";
 
 const TodoList = () => {
-  const todos = [
-    {
-      id: 1,
-      title: "Learn Java",
-      description: "Learn Java from scratch",
-      completed: false,
-      dueDate: "2025-03-25",
-    },
-    {
-      id: 2,
-      title: "Learn React",
-      description: "Learn React from scratch",
-      completed: true,
-      dueDate: "2025-03-25",
-    },
-    {
-      id: 3,
-      title: "Learn Python",
-      description: "Learn Python from scratch",
-      completed: false,
-      dueDate: "2025-03-25",
-    },
-    {
-      id: 4,
-      title: "Learn JavaScript",
-      description: "Learn JavaScript from scratch",
-      completed: true,
-      dueDate: "2025-03-25",
-    },
-    {
-      id: 5,
-      title: "Learn C++",
-      description: "Learn C++ from scratch",
-      completed: false,
-      dueDate: "2025-03-25",
-    },
-  ];
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetAllTodos();
+  }, []);
+
+  function fetAllTodos() {
+    getAllTodos()
+      .then((response) => setTodos(response.data))
+      .catch((error) => console.error(error));
+  }
 
   return (
     <div className="font-mono">
